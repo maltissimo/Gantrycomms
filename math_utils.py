6a58c0a891004a326508a5a041a4751b01f0f384
maltissimo <matteo.altissimo@elettra.eu>
2023-07-11 15:35:49 +0200

    11 July 2023

--- math_utils.py
+++ math_utils.py
@@ -2,5 +2,5 @@ import numpy as np
 from scipy import ndimage
 
 def get_centroid (ndarray):
-    centroid = ndimage.measurements.center_of_mass((ndarray))
+    centroid = ndimage.center_of_mass((ndarray))
     return (centroid)
